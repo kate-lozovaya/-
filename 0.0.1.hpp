780ff08a9943f87e2d60954ffa5d68a49d0be9d1
kate-lozovaya <katekalozovaya@gmail.com>
2017-10-09 11:25:17 +0300

    Update 0.0.1.hpp

--- 0.0.1.hpp
+++ 0.0.1.hpp
@@ -8,20 +8,16 @@ struct Node
 
 Node * reverse(Node * head)
 {
-	if (head != nullptr)
-	{
-		Node * cur = nullptr;
-		Node * prev = nullptr;
-		Node * next = nullptr;
+	Node * cur = head;
+	Node * prev = nullptr;
+	Node * next = nullptr;
 
-		cur = head;
-		while (cur)
-		{
-			next = cur->next;
-			cur->next = prev;
-			prev = cur;
-			cur = next;
-		}
-		head = prev;
+	while (cur)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
 	}
+	return prev;
 }
